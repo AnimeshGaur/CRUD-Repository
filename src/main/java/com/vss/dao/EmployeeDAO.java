7c0745d16c82a6c91655ea7c27b01f90440607ca
animesh gaur <ani8222@gmail.com>
2019-10-16 13:05:37 +0530

    intial commit

---
+++ src/main/java/com/vss/dao/EmployeeDAO.java
@@ -0,0 +1,13 @@
+package com.vss.dao;
+
+import java.util.List;
+import com.vss.model.Employee;
+
+public interface EmployeeDAO {
+
+	public void addEmployee(Employee employee);
+	public List<Employee> getAllEmployees();
+	public void deleteEmployee(Integer employeeId);
+	public Employee updateEmployee(Employee employee);
+	public Employee getEmployee(int employeeid);
+}
